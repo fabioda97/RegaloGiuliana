53c3169eddd09db996983be6bdd5a093120eeb44
fabioda97 <156315496+fabioda97@users.noreply.github.com>
2024-06-20 18:58:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function showSurprise() {
+    var giftImage = document.getElementById('giftImage');
+    giftImage.src = 'open-gift-box.png'; // Immagine del pacco aperto
     var surprise = document.getElementById('surprise');
     surprise.classList.remove('hidden');
 }
