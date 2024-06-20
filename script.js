07440d29beb3e5f6e308e5519baffa8fcc92175d
fabioda97 <156315496+fabioda97@users.noreply.github.com>
2024-06-20 19:34:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function showSurprise() {
-    var giftImage = document.getElementById('giftImage');
-    giftImage.src = 'open-gift-box.png'; // Immagine del pacco aperto
-    var surprise = document.getElementById('surprise');
-    surprise.classList.remove('hidden');
+function showPDF() {
+    var pdfWindow = window.open("", "_blank");
+    pdfWindow.location.href = "viaggio.pdf"; // Sostituisci con il percorso del tuo file PDF
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    var audio = document.getElementById("backgroundMusic");
+    var musicMessage = document.getElementById("musicMessage");
+
+    // Mostra il messaggio per far partire la musica
+    musicMessage.classList.remove('hidden');
+
+    // Aggiungi un event listener per avviare la musica al primo clic
+    document.addEventListener("click", function() {
+        audio.play().then(function() {
+            // Nascondi il messaggio una volta che la musica è partita
+            musicMessage.classList.add('hidden');
+        }).catch(function(error) {
+            console.log("Autoplay prevented. Please click somewhere to start the music.");
+        });
+    }, { once: true });
+});
