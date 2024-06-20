a6ed2fa9d515d035a075b52775a9fcac762a2aa3
fabioda97 <156315496+fabioda97@users.noreply.github.com>
2024-06-20 19:51:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function showPDF() {
     // Assicurati che il percorso al file PDF sia corretto
-    var pdfWindow = window.open("viaggio.pdf", "_blank");
+    var pdfWindow = window.open("Viaggio.pdf", "_blank");
 }
